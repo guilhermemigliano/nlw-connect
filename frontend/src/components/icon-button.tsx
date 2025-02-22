a19540cd7b916c017dcd4eb3f73023f3db038f65
Guilherme Migliano <guilherme@MacBook-Air-de-Guilherme.local>
2025-02-21 21:36:24 -0300

    desenvolvimento aula-2

--- frontend/src/components/icon-button.tsx
+++ frontend/src/components/icon-button.tsx
@@ -1,11 +1,16 @@
-import { type ComponentProps, ReactNode } from 'react'
+import { type ComponentProps, ReactNode } from 'react';
+import { twMerge } from 'tailwind-merge';
 
-interface IconButtonProps extends ComponentProps<'button'> {}
-export function IconButton(props: IconButtonProps) {
+interface ButtonProps extends ComponentProps<'button'> {}
+
+export function IconButton({ className, ...props }: ButtonProps) {
   return (
     <button
-      className="p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer hover:bg-blue hover:text-gray-900 transition-colors duration-300"
+      className={twMerge(
+        'p-1.5 bg-gray-500 text-blue rounded-md cursor-pointer transition-colors duration-300 hover:bg-blue hover:text-gray-900',
+        className,
+      )}
       {...props}
     />
-  )
+  );
 }
